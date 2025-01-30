1f20d1db4201ffff38d4ce4822da96950b88473f
Owais Akhtar <owais.akhtar@qt.io>
2025-01-30 14:37:51 +0200

    initial change

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,70 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>e', function()
+  require('neo-tree.command').execute { toggle = true }
+end)
+
+vim.api.nvim_set_keymap('n', '<F4>', [[:lua SwitchHeaderSource()<CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', 'fr', function()
+  require('telescope.builtin').lsp_references()
+end, { noremap = true, silent = true })
+
+function SwitchHeaderSource()
+  local file = vim.fn.expand '%'
+  local extension = vim.fn.expand '%:e'
+
+  if extension == 'h' then
+    vim.cmd('e ' .. file:gsub('%.h$', '.cpp'))
+  elseif extension == 'hpp' then
+    vim.cmd('e ' .. file:gsub('%.hpp$', '.cpp'))
+  elseif extension == 'cpp' then
+    vim.cmd('e ' .. file:gsub('%.cpp$', '.h'))
+  else
+    print 'Not a header or source file'
+  end
+end
+
+return {
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {},
+    config = function()
+      vim.keymap.set('n', '<leader>sb', function()
+        require('fzf-lua').buffers()
+      end, { desc = '[S]earch [B]uffers' })
+      vim.keymap.set('n', '<leader>sf', function()
+        require('fzf-lua').files()
+      end, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader><leader>', function()
+        require('fzf-lua').oldfiles()
+      end, { desc = 'Search Recent' })
+      vim.keymap.set('n', '<leader>sg', function()
+        require('fzf-lua').grep()
+      end, { desc = '[S]earch [G]rep' })
+      vim.keymap.set('n', '<leader>sr', function()
+        require('fzf-lua').lsp_references()
+      end, { desc = '[S]earch [R]eference' })
+
+      -- vim.api.nvim_set_keymap('n', '<leader>sp', [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
+      -- vim.api.nvim_set_keymap('n', '<leader>sl', [[<Cmd>lua require"fzf-lua".live_grep_glob()<CR>]], {})
+      -- vim.api.nvim_set_keymap('n', '<leader>sg', [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+      -- vim.api.nvim_set_keymap('n', '<F1>', [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
+    end,
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+}
